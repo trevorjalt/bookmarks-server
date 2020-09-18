2d887afea9e38cf70bfd82246d184ca9673f6a7f
Trevor Alt <trevorjalt@gmail.com>
2020-09-18 14:36:33 -0400

    shipping

---
+++ src/app.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
+const validateBearerToken = require('./validate-bearer-token')
+const errorHandler = require('./error-handler')
+const bookmarksRouter = require('./bookmarks/bookmarks-router')
+
+
+const app = express();
+
+const morganOption = (NODE_ENV === 'production')
+  ? 'dev'
+  : 'common'
+
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
+
+app.use(validateBearerToken)
+
+app.use(bookmarksRouter)
+
+app.get('/', (req, res) => {
+    res.send('Hello, world!');
+})
+
+app.use(errorHandler)
+
+module.exports = app
